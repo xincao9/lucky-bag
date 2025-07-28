7bbbf6f4fb72d53cbde35f2390a5b7cf48370f1d
fochat666 <fochat666@gmail.com>
2025-07-28 17:57:31 +0800

    OK

---
+++ src/main/java/fun/golinks/lucky/bag/LuckyBagApplication.java
@@ -0,0 +1,13 @@
+package fun.golinks.lucky.bag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyBagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyBagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/golinks/lucky/bag/LuckyBagApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.golinks.lucky.bag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyBagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
